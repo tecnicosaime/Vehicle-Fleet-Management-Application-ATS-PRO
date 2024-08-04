c39e1d593848121bb1d8131ef6601dfe81fc10ae
Anar Mamedov <anar26271@gmail.com>
2024-08-04 14:03:06 +0300

    update

--- src/Test.jsx
+++ src/Test.jsx
@@ -1,34 +1,68 @@
-import React, { useEffect, useState } from "react";
-import { t } from "i18next";
-import KmLog from "../../../../../components/table/KmLog.jsx";
-import { Button, Modal } from "antd";
-import { GetVehicleByIdService } from "../../../../../../api/services/vehicles/vehicles/services.jsx";
-import dayjs from "dayjs";
-import { GetDocumentsByRefGroupService, GetPhotosByRefGroupService } from "../../../../../../api/services/upload/services.jsx";
+import { useState, useEffect } from "react";
+import { Controller, useFormContext } from "react-hook-form";
+import PropTypes from "prop-types";
+import { Select } from "antd";
+import { CodeControlByUrlService } from "../../../../api/services/code/services";
 
-function KmTakibi({ visible, onClose, ids }) {
-  const [dataSource, setDataSource] = useState([]);
-  const [dataStatus, setDataStatus] = useState(false);
+const Driver = ({ name, codeName, required }) => {
+  const [data, setData] = useState([]);
+  const { watch, setValue, control } = useFormContext();
 
   useEffect(() => {
-    GetVehicleByIdService(ids).then((res) => {
-      setDataSource(res.data);
-    });
-  }, [ids, status, dataStatus]);
+    handleClickSelect();
+  }, []);
 
-  const footer = [
-    <Button key="back" className="btn cancel-btn" onClick={onClose}>
-      {t("kapat")}
-    </Button>,
-  ];
+  const handleClickSelect = async () => {
+    try {
+      const res = await CodeControlByUrlService("Driver/GetDriverListForSelectInput");
+      setData(res.data);
+    } catch (error) {
+      console.error("Error fetching driver list:", error);
+    }
+  };
 
   return (
-    <div>
-      <Modal title={`${t("kilometreGuncellemeGecmisi")}: ${dataSource?.plaka}`} open={visible} onCancel={onClose} maskClosable={false} footer={footer} width={1200}>
-        <KmLog data={dataSource} setDataStatus={setDataStatus} />
-      </Modal>
-    </div>
+    <Controller
+      name={codeName || "surucuId"}
+      control={control}
+      rules={{ required: required ? "Bu alan boş bırakılamaz!" : false }}
+      render={({ field, fieldState }) => (
+        <>
+          <Select
+            {...field}
+            showSearch
+            allowClear
+            optionFilterProp="children"
+            className={fieldState.error ? "input-error" : ""}
+            filterOption={(input, option) => (option?.label.toLowerCase() ?? "").includes(input.toLowerCase())}
+            filterSort={(optionA, optionB) => (optionA?.label.toLowerCase() ?? "").localeCompare(optionB?.label.toLowerCase() ?? "")}
+            options={data.map((item) => ({
+              label: item.isim,
+              value: item.surucuId,
+            }))}
+            value={watch(name || "surucu")}
+            onChange={(e) => {
+              field.onChange(e);
+              const selectedOption = data.find((option) => option.surucuId === e);
+              if (selectedOption) {
+                setValue(name || "surucu", selectedOption.isim);
+              } else {
+                setValue(name || "surucu", "");
+              }
+            }}
+            onClick={handleClickSelect}
+          />
+          {fieldState.error && <span style={{ color: "red" }}>{fieldState.error.message}</span>}
+        </>
+      )}
+    />
   );
-}
+};
 
-export default KmTakibi;
+Driver.propTypes = {
+  name: PropTypes.string,
+  codeName: PropTypes.string,
+  required: PropTypes.bool,
+};
+
+export default Driver;
