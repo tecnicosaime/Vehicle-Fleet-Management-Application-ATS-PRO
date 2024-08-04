b9ca515fa151aac389ed7ca8879122a6aa22dcc7
Anar Mamedov <anar26271@gmail.com>
2024-08-04 22:26:35 +0300

    test

---
+++ src/_root/pages/Hazirlaniyor.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { Button, Result } from "antd";
+
+export default function NotFound() {
+  const handleGoHome = () => {
+    window.location.href = "/"; // Ana sayfanın URL'sini buraya yazın
+  };
+
+  return (
+    <div
+      style={{
+        height: "calc(100vh - 210px)",
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+      }}>
+      <Result
+        status="404"
+        title="Hazırlanıyor!"
+        subTitle="Bu sayfa henüz hazırlanma aşamasındadır."
+        extra={
+          <Button type="primary" onClick={handleGoHome}>
+            Ana Sayfaya Dön
+          </Button>
+        }
+      />
+    </div>
+  );
+}
