ee253ff0227f017f6d6e7366371d5a0cf5863c19
Anar Mamedov <anar26271@gmail.com>
2024-10-06 19:09:34 +0300

    yakıt analizleri bitti

--- src/_root/pages/Analizler/YakitTuketimAnalizi/Main/components/LokasyonBazindaYakitTuketimleri.jsx
+++ src/_root/pages/Analizler/YakitTuketimAnalizi/Main/components/LokasyonBazindaYakitTuketimleri.jsx
@@ -172,8 +172,8 @@ function LokasyonBazindaYakitTuketimleri(props) {
   const baslangicTarihi = watch("baslangicTarihi");
   const bitisTarihi = watch("bitisTarihi");
 
-  const startYear = baslangicTarihi;
-  const endYear = bitisTarihi;
+  const startYear = baslangicTarihi ? dayjs(baslangicTarihi).year() : 0;
+  const endYear = bitisTarihi ? dayjs(bitisTarihi).year() : 0;
 
   const fetchData = async () => {
     setIsLoading(true);
