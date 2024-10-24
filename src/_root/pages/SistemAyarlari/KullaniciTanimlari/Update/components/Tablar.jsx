40b500774d44cdadb00f0f29d66a52e11455b505
Anar Mamedov <anar26271@gmail.com>
2024-10-24 22:19:03 +0400

    kullanıcı güncelleme ekranı radio grupla tabs yöntemi

--- src/_root/pages/SistemAyarlari/KullaniciTanimlari/Update/components/Tablar.jsx
+++ src/_root/pages/SistemAyarlari/KullaniciTanimlari/Update/components/Tablar.jsx
@@ -1,29 +1,30 @@
-import React from "react";
-import { Tabs } from "antd";
+import React, { useState } from "react";
+import { Radio, Divider } from "antd";
 import MainTabs from "./MainTabs/MainTabs";
 import Yetkiler from "./Yetkiler/Yetkiler";
 import LokasyonYetkileri from "./LokasyonYetkileri/LokasyonYetkileri";
 import { t } from "i18next";
 
 function Tablar() {
-  const items = [
-    {
-      key: "1",
-      label: t("hesap"),
-      children: <MainTabs />,
-    },
-    {
-      key: "2",
-      label: t("lokasyonlar"),
-      children: <LokasyonYetkileri />,
-    },
-    {
-      key: "3",
-      label: t("yetkiler"),
-      children: <Yetkiler />,
-    },
-  ];
-  return <Tabs defaultActiveKey="1" items={items} />;
+  const [tabKey, setTabKey] = useState("1");
+
+  const handleTabChange = (e) => {
+    setTabKey(e.target.value);
+  };
+
+  return (
+    <>
+      <Radio.Group value={tabKey} onChange={handleTabChange} style={{ display: "flex", justifyContent: "center" }}>
+        <Radio.Button value="1">{t("hesap")}</Radio.Button>
+        <Radio.Button value="2">{t("lokasyonlar")}</Radio.Button>
+        <Radio.Button value="3">{t("yetkiler")}</Radio.Button>
+      </Radio.Group>
+      <Divider style={{ marginBottom: 10 }} />
+      {tabKey === "1" && <MainTabs />}
+      {tabKey === "2" && <LokasyonYetkileri />}
+      {tabKey === "3" && <Yetkiler />}
+    </>
+  );
 }
 
 export default Tablar;
