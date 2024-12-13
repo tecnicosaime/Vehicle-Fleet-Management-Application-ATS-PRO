80f17f38950079a23a4f9a98ea3ba8d12052fed1
Anar Mamedov <anar26271@gmail.com>
2024-12-13 19:16:54 +0400

    rapor tablosunda düzenleme

--- src/_root/pages/raporlar/RaporTabs/components/RaporModal/DraggableRow.jsx
+++ src/_root/pages/raporlar/RaporTabs/components/RaporModal/DraggableRow.jsx
@@ -3,10 +3,9 @@ import { useSortable } from "@dnd-kit/sortable";
 import { CSS } from "@dnd-kit/utilities";
 import { HolderOutlined } from "@ant-design/icons";
 
-const DraggableRow = ({ id, text, index, moveRow, className, style, visible, onVisibilityChange, ...restProps }) => {
+const DraggableRow = ({ id, text, ...restProps }) => {
   const { attributes, listeners, setNodeRef, transform, transition, isDragging } = useSortable({ id });
   const styleWithTransform = {
-    ...style,
     transform: CSS.Transform.toString(transform),
     transition,
     opacity: isDragging ? 0.5 : 1,
@@ -14,24 +13,12 @@ const DraggableRow = ({ id, text, index, moveRow, className, style, visible, onV
     display: "flex",
     alignItems: "center",
     gap: 8,
+    cursor: "grab",
   };
 
   return (
-    <div ref={setNodeRef} style={styleWithTransform} {...restProps} {...attributes}>
-      {/* <Checkbox
-          checked={visible}
-          onChange={(e) => onVisibilityChange(index, e.target.checked)}
-          style={{ marginLeft: "auto" }}
-        /> */}
-      <div
-        {...listeners}
-        style={{
-          cursor: "grab",
-          flexGrow: 1,
-          display: "flex",
-          alignItems: "center",
-        }}
-      >
+    <div ref={setNodeRef} style={styleWithTransform} {...attributes}>
+      <div {...listeners} style={{ display: "flex", alignItems: "center", flexGrow: 1 }}>
         <HolderOutlined style={{ marginRight: 8 }} />
         {text}
       </div>
