4fff7e8d8db7d1e84920685b838af324ad6dfb3f
Anar Mamedov <anar26271@gmail.com>
2025-01-26 00:25:11 +0400

    sigortalara filtreler eklendi

---
+++ src/_root/pages/vehicles-control/sigorta/filter/Filters.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+import ConditionFilter from "./ConditionFilter";
+import LocationFilter from "./LocationFilter";
+import TypeFilter from "./TypeFilter";
+import CustomFilter from "./custom-filter/CustomFilter";
+
+export default function Filters({ onChange }) {
+  const [filters, setFilters] = React.useState({
+    lokasyonlar: {},
+    isemritipleri: {},
+    durumlar: {},
+    customfilter: {},
+  });
+
+  React.useEffect(() => {
+    onChange("filters", filters);
+  }, [filters, onChange]);
+
+  return (
+    <>
+      {/* <TypeFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, isemritipleri: newFilters }))} /> */}
+      {/* <ConditionFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, durumlar: newFilters }))} /> */}
+      {/* <LocationFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, lokasyonlar: newFilters }))} /> */}
+      <CustomFilter onSubmit={(newFilters) => setFilters((state) => ({ ...state, customfilters: newFilters }))} />
+    </>
+  );
+}
